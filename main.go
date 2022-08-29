1762d192d86aaf3d054fdd6002999c3cccc18e9c
Hafezd El Daffa <hafezdeldaffa9@gmail.com>
2022-08-29 23:37:25 +0700

    edit: refactor connect to db string using .env

--- main.go
+++ main.go
@@ -5,14 +5,28 @@ import (
 	"bwastartup/user"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
+func goDotEnvVariable(key string) string {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading get .env file")
+	}
+
+	return os.Getenv(key)
+}
+
 func main() {
-	dsn := "root:Hafezd123@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+	userAccount := goDotEnvVariable("USER")
+	password := goDotEnvVariable("PASSWORD")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local", userAccount, password)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err.Error())
