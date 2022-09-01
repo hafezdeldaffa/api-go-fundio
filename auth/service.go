f31c9b5093981c11b63b9d9a9336f335d6f2daf6
Hafezd El Daffa <hafezdeldaffa9@gmail.com>
2022-09-01 17:58:06 +0700

    add: validate token func

--- auth/service.go
+++ auth/service.go
@@ -2,12 +2,14 @@ package auth
 
 import (
 	"bwastartup/helper"
+	"errors"
 
 	"github.com/golang-jwt/jwt/v4"
 )
 
 type Service interface {
 	GenerateToken(userID int) (string, error)
+	ValidateToken(token string) (*jwt.Token, error)
 }
 
 type jwtService struct {
@@ -17,13 +19,13 @@ func NewJWTService() *jwtService {
 	return &jwtService{}
 }
 
+var secretKey = helper.GoDotEnvVariable("SECRET_KEY")
+
 func (s *jwtService) GenerateToken(userID int) (string, error) {
 	claim := jwt.MapClaims{}
 	claim["user_id"] = userID
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
-
-	var secretKey = helper.GoDotEnvVariable("SECRET_KEY")
 
 	signedToken, err := token.SignedString([]byte(secretKey))
 	if err != nil {
@@ -31,4 +33,21 @@ func (s *jwtService) GenerateToken(userID int) (string, error) {
 	}
 
 	return signedToken, nil
+}
+
+func (s *jwtService) ValidateToken(encodedToken string) (*jwt.Token, error) {
+	token, err := jwt.Parse(encodedToken, func(t *jwt.Token) (interface{}, error) {
+		_, ok := t.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, errors.New("invalid Token")
+		}
+
+		return []byte(secretKey), nil
+	})
+
+	if err != nil {
+		return token, err
+	}
+
+	return token, nil
 }
